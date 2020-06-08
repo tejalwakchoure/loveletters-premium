a80026fd373bf815a9369467fac81e2c5bbcb5b7
Tejal Wakchoure <tejal.wakchoure@gmail.com>
2020-06-08 19:06:37 +0530

    Update values.js

--- react/game/assets/values.js
+++ react/game/assets/values.js
@@ -1,8 +1,18 @@
 export const Values = {
 	"num_players" : 3,
+	"all_players" : ["p1", "p2", "p3"],
 	"all_cards" : ["Jester", "Assassin", "Guard", "Priest", "Princess", "Baron"],
+	"player_points" : {
+		"p1" : 0,
+		"p2" : 0,
+		"p3" : 0
+	},
 	"played_cards" : ["Guard", "Priest", "Princess"],
-	"current_cards" : ["Priest", "Jester", "Baron"],
-	"current_player" : 1,
+	"current_cards" : {
+		"p1" : "Priest", 
+		"p2" : "Jester", 
+		"p3" : "Baron"
+	},
 	"draw_pile" : ["Assassin", "Guard", "Priest", "Baron"],
+
 };
