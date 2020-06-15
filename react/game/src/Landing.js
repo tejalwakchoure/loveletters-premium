51d584e89d2d41c3ee1a91b9c448691794e45695
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-06-16 03:33:44 +0530

    Finishing touches for basic gameplay

--- react/game/src/Landing.js
+++ react/game/src/Landing.js
@@ -15,7 +15,6 @@ class Landing extends React.Component {
 	    	showStartButton: false, // true for local testing, false for global
 	    	userID: ' ',
 	    	username: ' ',
-	    	host: ' ',
 	    	gameStatus: 0
 	    };
 	   	this.getPlayers = this.getPlayers.bind(this);
@@ -28,8 +27,7 @@ class Landing extends React.Component {
 		this.setState({
 			all_players: obj.plyrs,
 			userID: obj.uid,
-			username: obj.username,
-			host: obj.host
+			username: obj.username
 		});
 		if(obj.uid === obj.host)
 			this.setState({showStartButton: true});
