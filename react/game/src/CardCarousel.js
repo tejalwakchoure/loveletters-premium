835214745486dcff72280a71e047b8a0bc94009d
tejalwakchoure <tejal.wakchoure@gmail.com>
2020-06-19 01:47:55 +0530

    Fixes

--- react/game/src/CardCarousel.js
+++ react/game/src/CardCarousel.js
@@ -1,34 +1,42 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.css';
-import SizeMe from 'react-sizeme';
 import './Game.css';
 
 class CardCarousel extends React.Component {
+	
 	getCard(name) {
 		return require('../assets/cards/mini'+name+'.png');
-	}	
+	}
+
+	scrollToBottom = () => {
+		this.lastCard.scrollIntoView({ behavior: "smooth" });
+	}
+
+	componentDidUpdate () {
+		this.scrollToBottom();
+	}
+
 	render() {
 		let playedCardlist = [];
 		if(this.props.allCardsDiscarded!==undefined)
 			playedCardlist = this.props.allCardsDiscarded;
-		const { width, height } = this.props.size;
-		const cardWidth = (width<window.innerWidth)?'3.7vw': '3.1vw';
 		return(
 		  	<div className='Card-carousel'> 
 			  	{playedCardlist.map((item, index) => 
 			  		<div key={item}>
-                    	<Card style={{ width: cardWidth, marginLeft: '1px' }}>
+                    	<Card style={{ width: '3.7vw', marginLeft: '1px' }}>
 					      <Card.Body style={{ padding: 0 }}>
 					        <Card.Img src={this.getCard(item)}/>
 					      </Card.Body>
 					    </Card>
 					</div>
 				)}
+				<div id="last-card" ref={(e) => {this.lastCard=e;}}></div>
 		  	</div>
 		);
 	}
 }
 
-export default SizeMe()(CardCarousel);
+export default CardCarousel;
 
