06c6cba0c962bc4dc024c718445c0cfea0aec04a
MayankKumar1123 <mayank.kumar1123@gmail.com>
2020-06-23 20:37:43 +0530

    More Admins Fixe

--- react/game/src/socket-context.js
+++ react/game/src/socket-context.js
@@ -1,7 +1,11 @@
 import { w3cwebsocket as W3CWebSocket } from "websocket"
 
 const socket = new W3CWebSocket(((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + "/ws")
-
+socket.onclose = () => {
+	console.log('WebSocket Client has closed');
+	//Try to reconnect here
+	
+};
 
 
 export default socket;
